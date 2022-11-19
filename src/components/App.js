b0a6c5d50405a5cfa54624eb5abc99f2dd7d2646
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-19 17:50:30 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main" data-ns-test>
+      <h2 data-ns-test="project-name" >  Project name</h2>
+      <p data-ns-test="project-name">project name</p>
+    </div>
   )
 }
 
