52ca2e5098101f0de16d5c19c05c8894424c350f
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-12-15 14:53:15 +0530

    k

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React, { Component, useState } from "react";
+import React, { component, useState } from "react";
 import '../styles/App.css';
-
 const App = () => {
   return (
-    <div id="main" data-ns-test>
-      <h2 data-ns-test="project-name" >  Project name</h2>
-      <p data-ns-test="project-name">project name</p>
+    <div id="main">
+      <h3 data-ns-test="project-name">Ride Pooling Project</h3>
+      <p data-ns-test="project-description">The passenger is assigned to a free vehicle by the system, and at the same time, similar requests coming in via the app are checked. If several people would like to go in the same direction and can be transported without a long wait or major detours, the algorithm assigns them to the same trip.</p>
     </div>
   )
 }
